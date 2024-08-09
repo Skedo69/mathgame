474179d2efc933c0d8fee2bc802794daa481ae3d
Skedo69 <ahmedsoaida2@gmail.com>
2024-08-10 00:00:48 +0300

    Update bla.js

--- bla.js
+++ bla.js
@@ -2,13 +2,16 @@ let num = 0;
 let num2 = 0;
 let Streak = 0;
 let wins =0;
-
+let run = false;
 function Generate(){
-    num = Math.floor(Math.random()*100+1)
-    num2 = Math.floor(Math.random()*100+1)
-    document.getElementById("header").textContent = `${num} X ${num2}`
-    console.log(num*num2)
-    document.getElementById("value2").value = ""
+    if(!run){
+        num = Math.floor(Math.random()*100+1)
+        num2 = Math.floor(Math.random()*100+1)
+        document.getElementById("header").textContent = `${num} X ${num2}`
+        console.log(num*num2)
+        document.getElementById("value2").value = ""
+        run = true;
+    }
 }
 function Checker(){
     if(num*num2 == document.getElementById("value2").value){
@@ -19,10 +22,13 @@ function Checker(){
         num2 = Math.floor(Math.random()*100+1)
         document.getElementById("strok").textContent = `Your Streak is: ${Streak}🔥`
         document.getElementById("won").textContent = `Your Wins is: ${wins}`
+        run = false;
+        run = false;
     }
     else{
         Streak = 0;
         document.getElementById("header").textContent = `7mar 😢, the number was ${num*num2}`
         document.getElementById("strok").textContent = `Your Streak is: ${Streak}`
+        run = false;
     }
 }
